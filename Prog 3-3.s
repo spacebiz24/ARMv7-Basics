b02470803d8e0894feac5d2543344339223716d2
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-03-19 15:15:07 +0530

    Create Prog 3-3.s

---
+++ Prog 3-3.s
@@ -0,0 +1,22 @@
+@ Count the number of 1s and 0s in a word
+
+.data
+	num:.word 0xFFFFFFFF
+
+.text
+.global _start
+_start:
+	LDR R0, =num
+	LDR R0, [R0]
+	MOV R1, #0
+	
+LOOP:
+	LSRS R0,R0,#1
+	ADDCS R1,R1,#1
+	CMP R0,#0x0
+	BNE LOOP
+	
+	SUB R2,R1,#0x20
+	
+stop: BAL stop
+.end
