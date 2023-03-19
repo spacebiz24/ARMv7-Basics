7a04bbab663875e6a15b012d7745326f2ed1d810
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-03-19 15:12:46 +0530

    Create Prog 2-1.s

---
+++ Prog 2-1.s
@@ -0,0 +1,27 @@
+@ Add and subtract 2 words
+
+.data
+	num1: .word 0x80000555, 0xFFFFFFFF
+	num2: .word 0xFFFFFFFF, 0x80000555
+	res: .space 4*2,1
+
+.text
+.global _start
+_start:
+	LDR R0,=num1
+	LDR R1,=num2
+	LDR R2,=res
+	LDR R3,[R0],#0x4
+	LDR R4,[R1],#0x4
+	MOV R7,#1
+	
+	ADDS R5,R3,R4
+	LDR R3,[R0]
+	LDR R4,[R1]
+	ADCS R6,R3,R4
+	STMEA R2!,{R5,R6}
+	STRCS R7,[R2]
+	
+stop:
+	BAL stop
+.end
