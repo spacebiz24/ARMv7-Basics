3b79ca7e5faea3cfb887f6146ab3d91ff48b46a5
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-03-19 15:16:58 +0530

    Create Prog 5-1.asm

---
+++ Prog 5-1.asm
@@ -0,0 +1,24 @@
+@ Calculate the factorial of a number
+
+.data
+	num: .word #0x4
+	fact: .word
+
+.text
+.global _start
+_start:
+	LDR R0, =num
+	LDR R0,[R0]
+	LDR R1, =fact
+	MOV R2, R0
+	
+loop:
+	SUBS R0,R0,#1
+	MULNE R2,R2,R0
+	BNE loop
+	
+	STR R1,[R1]
+	
+stop:
+	BAL stop
+.end
