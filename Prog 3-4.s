f2b54091c22df2156a88cf12ab85a3a91eb39f9d
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-04-27 18:22:11 +0530

    Update Prog 3-4.s

--- Prog 3-4.s
+++ Prog 3-4.s
@@ -10,7 +10,7 @@ _start:
 	LDR R0,[R0]
 	MOV R1,#0x1
 	
-	BIC R0,R0,R1,LSL #0x2 @ Clearing the 4th bit
+	BIC R0,R0,R1,LSL #0x2 @ Clearing the 3th bit
 	ORR R0,R0,R1,LSL #0x8 @ Setting the 7th bit
 	EOR R0,R0,R1,LSL #0x1 @ Complimenting the 2nd bit
 	TST R0,#0xA @ Testing if R0 is 0xA
